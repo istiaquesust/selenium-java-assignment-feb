1fc71e557634babc7827d4cda5b731378706cdd7
Istiaque Ahmed <istiaque.sust@gmail.com>
2021-03-04 04:28:52 +0600

    update setup file

--- src/assignment_2/setup/DriverSetup.java
+++ src/assignment_2/setup/DriverSetup.java
@@ -2,7 +2,6 @@ package assignment_2.setup;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
